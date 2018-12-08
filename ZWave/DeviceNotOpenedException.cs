5a01bf2ee846970541e5f068c5fc809a14fb7081
Patrik Pfaffenbauer <patrik.pfaffenbauer@netxautomation.com>
2018-12-08 11:18:48 +0100

    added message to exception

--- ZWave/DeviceNotOpenedException.cs
+++ ZWave/DeviceNotOpenedException.cs
@@ -4,6 +4,8 @@ namespace ZWave
 {
     public class DeviceNotOpenedException : Exception
     {
-
+        public DeviceNotOpenedException() : base($"Device is not opened, you need to call {nameof(ZWaveController.Open)} first")
+        {
+        }
     }
 }
